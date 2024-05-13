65107f9d8999f62c16b51183183acd75cb984588
Artem Korolev <artemkorolevv@mail.ru>
2024-05-13 16:18:50 +0400

    add babel,eslint

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,6 +3,10 @@ import webpack from 'webpack';
 import { BuildOptions } from './types/config';
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+  const svgLoader = {
+    test: /\.svg$/,
+    use: ['@svgr/webpack'],
+  };
   const cssLoader = {
     test: /\.s[ac]ss$/i,
     use: [
@@ -21,10 +25,30 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     ],
   };
 
+  const babelLoader = {
+    test: /\.(js|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+      },
+    },
+  };
+
   const typescriptLoader = {
     test: /\.tsx?$/,
     use: 'ts-loader',
     exclude: /node_modules/,
   };
-  return [typescriptLoader, cssLoader];
+
+  const fileLoader = {
+    test: /\.(png|jpe?g|gif)$/i, // можно добавить шрифты
+    use: [
+      {
+        loader: 'file-loader',
+      },
+    ],
+  };
+  return [fileLoader, svgLoader, typescriptLoader, cssLoader];
 }
---
+++ src/app/App.tsx
@@ -0,0 +1,29 @@
+import './styles/index.scss';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { AppRouter } from 'app/providers/router';
+import { Navbar } from 'widgets/Navbar';
+import { Sidebar } from 'widgets/Sidebar';
+import { Suspense } from 'react';
+
+export enum Theme {
+  LIGHT = 'light',
+  DARK = 'dark',
+}
+
+const App = () => {
+  const { theme } = useTheme();
+  return (
+    <div className={classNames('app', {}, [theme])}>
+      <Suspense fallback="">
+        <Navbar />
+        <div className="content-page">
+          <Sidebar />
+          <AppRouter />
+        </div>
+      </Suspense>
+    </div>
+  );
+};
+
+export default App;
